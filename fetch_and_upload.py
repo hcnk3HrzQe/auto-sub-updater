91607041a7c7859e3c5bb0ef5aafdb98063580d2
hcnk3HrzQe <39117042+hcnk3HrzQe@users.noreply.github.com>
2025-07-15 21:01:39 +0800

    Update fetch_and_upload.py

--- fetch_and_upload.py
+++ fetch_and_upload.py
@@ -1,1 +1,13 @@
+import requests
+   import os
 
+   # 抓取目标 URL
+   url = "https://api.wcc.best/sub?target=clash&url=http://8.149.134.37:12585/vmess/sub&insert=false&config=https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online.ini"
+   response = requests.get(url)
+   content = response.text
+
+   # 保存到文件
+   with open("sub.txt", "w") as f:
+       f.write(content)
+
+   print("文件已更新！")
